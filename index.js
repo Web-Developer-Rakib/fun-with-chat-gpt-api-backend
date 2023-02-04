0e7ebec61a22b119e669a9f700b846e67686043d
Web-Developer-Rakib <rakibodesk7@gmail.com>
2023-02-04 15:33:19 +0600

    API configuration done

---
+++ index.js
@@ -0,0 +1,43 @@
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+import { Configuration, OpenAIApi } from "openai";
+
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express();
+const port = 5000 || process.env.PORT;
+app.use(cors());
+app.use(express.json());
+
+app.post("/", async (req, res) => {
+  try {
+    const prompt = req.body.prompt;
+    console.log(prompt);
+
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error || "wrong");
+  }
+});
+
+app.listen(port, () => console.log(`Servr is runing on port ${port}`));
